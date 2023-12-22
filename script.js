0c11689dc7b4f41689b82093ee9aecbb967bff3a
Prateek <prateekbisht701@gmail.com>
2023-12-22 14:46:27 +0530

    Made a calculator using HTML-CSS-JS

---
+++ script.js
@@ -0,0 +1,34 @@
+const display = document.querySelector('.input');
+const buttons = document.querySelectorAll('.button');
+
+console.log(buttons)
+let string = '';
+
+buttons.forEach( button => {
+    
+    button.addEventListener('click', (e) => {
+        
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            display.value = string;
+
+            // if(string      = string){
+
+            // }
+        }
+        else if(e.target.innerHTML === 'AC'){
+            string = '';
+            display.value = string;
+        }
+        else if(e.target.innerHTML === 'DE'){
+            string = string.slice(0,-1)
+            display.value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            display.value = string;
+        }
+    })
+})
+
+//The eval() function evaluates JavaScript code represented as a string and returns its completion value.
